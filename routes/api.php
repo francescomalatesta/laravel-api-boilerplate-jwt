6e18282b58589aa9908037262a08f7d3f4a8ce8c
Francesco Malatesta <hellofrancesco@gmail.com>
2016-11-15 21:39:09 +0100

    Add basic versioning to API with related settings

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use Dingo\Api\Routing\Router;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+/** @var Router $api */
+$api = app(Router::class);
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+$api->version('v1', function (Router $api) {
+    // TODO: put v1 default routes here!
+});
