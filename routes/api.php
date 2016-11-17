befa5f4347168d9ec23c01b0461d78b627bb5852
Francesco Malatesta <hellofrancesco@gmail.com>
2016-11-17 17:46:23 +0100

    Add routes examples for every situation

--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,27 @@ $api->version('v1', function (Router $api) {
         $api->post('recovery', 'App\\Api\\V1\\Controllers\\ForgotPasswordController@sendResetEmail');
         $api->post('reset', 'App\\Api\\V1\\Controllers\\ResetPasswordController@resetPassword');
     });
+
+    $api->group(['middleware' => 'jwt.auth'], function(Router $api) {
+        $api->get('protected', function() {
+            return response()->json([
+                'message' => 'Access to this item is only for authenticated user. Provide a token in your request!'
+            ]);
+        });
+
+        $api->get('refresh', [
+            'middleware' => 'jwt.refresh',
+            function() {
+                return response()->json([
+                    'message' => 'By accessing this endpoint, you can refresh your access token at each request. Check out this response headers!'
+                ]);
+            }
+        ]);
+    });
+
+    $api->get('hello', function() {
+        return response()->json([
+            'message' => 'This is a simple example of item returned by your APIs. Everyone can see it.'
+        ]);
+    });
 });
