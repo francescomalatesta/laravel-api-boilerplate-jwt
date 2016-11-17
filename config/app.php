ffdfe87f11b2d31ae929cf1cfd9e3a184c811c2d
Francesco Malatesta <hellofrancesco@gmail.com>
2016-11-17 18:07:44 +0100

    Setup the CORS package and its integration

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
 
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
         Dingo\Api\Provider\LaravelServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
 
         /*
          * Application Service Providers...
