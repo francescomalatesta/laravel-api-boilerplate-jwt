6c80353521a86ee8a52a4b836807320172ebe0ce
Francesco Malatesta <hellofrancesco@gmail.com>
2016-11-17 17:45:18 +0100

    Add JWT package middlewares

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Http;
 
+use Tymon\JWTAuth\Middleware\RefreshToken;
+use Tymon\JWTAuth\Middleware\GetUserFromToken;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -52,5 +54,8 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+
+        'jwt.auth' => GetUserFromToken::class,
+        'jwt.refresh' => RefreshToken::class,
     ];
 }
