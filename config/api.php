8d9086c3aa3c3794c4183838cd909fd3da3e1ce8
Francesco Malatesta <hellofrancesco@gmail.com>
2016-11-17 09:58:37 +0100

    Define the default error format in config

--- config/api.php
+++ config/api.php
@@ -138,11 +138,13 @@ return [
     */
 
     'errorFormat' => [
-        'message' => ':message',
-        'errors' => ':errors',
-        'code' => ':code',
-        'status_code' => ':status_code',
-        'debug' => ':debug',
+        'error' => [
+            'message' => ':message',
+            'errors' => ':errors',
+            'code' => ':code',
+            'status_code' => ':status_code',
+            'debug' => ':debug'
+        ]
     ],
 
     /*
